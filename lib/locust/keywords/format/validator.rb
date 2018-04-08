09d8fa1c31879ccae38321a422ab55750aa539b1
nepalez <andrew.kozin@gmail.com>
2018-04-08 21:17:29 +0300

    Validate consistency of format and type

--- lib/locust/keywords/format/validator.rb
+++ lib/locust/keywords/format/validator.rb
@@ -6,6 +6,17 @@ class Locust::Keywords::Format
     validate :parent_is_an_object
     validate :source_is_a_string
     validate :source_is_not_empty
+    validate :integer_is_an_integer
+    validate :long_is_an_integer
+    validate :float_is_a_number
+    validate :double_is_a_number
+    validate :boolean_is_a_boolean
+    validate :byte_is_a_string
+    validate :binary_is_a_string
+    validate :password_is_a_string
+    validate :string_is_a_string
+    validate :date_is_a_string
+    validate :date_time_is_a_string
 
     private
 
@@ -23,6 +34,98 @@ class Locust::Keywords::Format
       return if errors.any?
       return unless source == ""
       errors << message("Its value MUST not be empty.")
+    end
+
+    def integer_is_an_integer
+      return if errors.any?
+      return unless source == "integer"
+      return if type == "integer"
+      errors << \
+        message("This format MAY be added to a schema describing an integer.")
+    end
+
+    def long_is_an_integer
+      return if errors.any?
+      return unless source == "long"
+      return if type == "integer"
+      errors << \
+        message("This format MAY be added to a schema describing an integer.")
+    end
+
+    def float_is_a_number
+      return if errors.any?
+      return unless source == "float"
+      return if type == "number"
+      errors << \
+        message("This format MAY be added to a schema describing a number.")
+    end
+
+    def double_is_a_number
+      return if errors.any?
+      return unless source == "double"
+      return if type == "number"
+      errors << \
+        message("This format MAY be added to a schema describing a number.")
+    end
+
+    def boolean_is_a_boolean
+      return if errors.any?
+      return unless source == "boolean"
+      return if type == "boolean"
+      errors << \
+        message("This format MAY be added to a schema describing a boolean.")
+    end
+
+    def byte_is_a_string
+      return if errors.any?
+      return unless source == "byte"
+      return if type == "string"
+      errors << \
+        message("This format MAY be added to a schema describing a string.")
+    end
+
+    def binary_is_a_string
+      return if errors.any?
+      return unless source == "binary"
+      return if type == "string"
+      errors << \
+        message("This format MAY be added to a schema describing a string.")
+    end
+
+    def password_is_a_string
+      return if errors.any?
+      return unless source == "password"
+      return if type == "string"
+      errors << \
+        message("This format MAY be added to a schema describing a string.")
+    end
+
+    def string_is_a_string
+      return if errors.any?
+      return unless source == "string"
+      return if type == "string"
+      errors << \
+        message("This format MAY be added to a schema describing a string.")
+    end
+
+    def date_is_a_string
+      return if errors.any?
+      return unless source == "date"
+      return if type == "string"
+      errors << \
+        message("This format MAY be added to a schema describing a string.")
+    end
+
+    def date_time_is_a_string
+      return if errors.any?
+      return unless source == "dateTime"
+      return if type == "string"
+      errors << \
+        message("This format MAY be added to a schema describing a string.")
+    end
+
+    def type
+      @type ||= parent.type&.source
     end
   end
 end
--- spec/unit/locust/keywords/format_spec.rb
+++ spec/unit/locust/keywords/format_spec.rb
@@ -1,6 +1,7 @@
 RSpec.describe Locust::Keywords::Format do
   let(:keyword) { described_class.call source, parent }
-  let(:parent)  { Locust::Keywords::Object.call(nil, nil) }
+  let(:parent)  { Locust::Keywords::Object.call(schema, nil) }
+  let(:schema)  { { "type" => "string" } }
   let(:source)  { "email" }
 
   describe ".call" do
@@ -30,6 +31,83 @@ RSpec.describe Locust::Keywords::Format do
 
     context "when parent is not an object" do
       let(:parent) { Locust::Schema.call(nil, nil) }
+
+      it { is_expected.not_to be_empty }
+    end
+
+    context "when type of integer format is not an integer" do
+      let(:schema) { { "type" => "string" } }
+      let(:source) { "integer" }
+
+      it { is_expected.not_to be_empty }
+    end
+
+    context "when type of long format is not an integer" do
+      let(:schema) { { "type" => "string" } }
+      let(:source) { "long" }
+
+      it { is_expected.not_to be_empty }
+    end
+
+    context "when type of float format is not a number" do
+      let(:schema) { { "type" => "string" } }
+      let(:source) { "float" }
+
+      it { is_expected.not_to be_empty }
+    end
+
+    context "when type of double format is not a number" do
+      let(:schema) { { "type" => "string" } }
+      let(:source) { "double" }
+
+      it { is_expected.not_to be_empty }
+    end
+
+    context "when type of string format is not a string" do
+      let(:schema) { { "type" => "integer" } }
+      let(:source) { "string" }
+
+      it { is_expected.not_to be_empty }
+    end
+
+    context "when type of byte format is not a string" do
+      let(:schema) { { "type" => "integer" } }
+      let(:source) { "byte" }
+
+      it { is_expected.not_to be_empty }
+    end
+
+    context "when type of binary format is not a string" do
+      let(:schema) { { "type" => "integer" } }
+      let(:source) { "binary" }
+
+      it { is_expected.not_to be_empty }
+    end
+
+    context "when type of boolean format is not a boolean" do
+      let(:schema) { { "type" => "integer" } }
+      let(:source) { "boolean" }
+
+      it { is_expected.not_to be_empty }
+    end
+
+    context "when type of date format is not a string" do
+      let(:schema) { { "type" => "integer" } }
+      let(:source) { "date" }
+
+      it { is_expected.not_to be_empty }
+    end
+
+    context "when type of dateTime format is not a string" do
+      let(:schema) { { "type" => "integer" } }
+      let(:source) { "dateTime" }
+
+      it { is_expected.not_to be_empty }
+    end
+
+    context "when type of password format is not a string" do
+      let(:schema) { { "type" => "integer" } }
+      let(:source) { "password" }
 
       it { is_expected.not_to be_empty }
     end
